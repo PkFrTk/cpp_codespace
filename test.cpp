a4a662d6817f6e6000fa1ca07eba7aebace57f66
PkFrTk <1700013007@qq.com>
2023-02-15 07:44:05 +0000

    Commit test

--- test.cpp
+++ test.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
+
 class Building;
 class goodGay
 {
 public:
 
 	goodGay();
-	void visit();
+	void visit(); //只让visit函数作为Building的好朋友，可以发访问Building中私有内容
+	void visit2(); 
 
 private:
 	Building *building;
@@ -15,9 +17,10 @@ private:
 
 class Building
 {
-	//告诉编译器 goodGay类是Building类的好朋友，可以访问到Building类中私有内容
+	//告诉编译器  goodGay类中的visit成员函数 是Building好朋友，可以访问私有内容
+	// friend void goodGay::visit();
+	// friend void goodGay::visit2();
 	friend class goodGay;
-
 public:
 	Building();
 
@@ -40,21 +43,26 @@ goodGay::goodGay()
 
 void goodGay::visit()
 {
-	cout << "好基友正在访问" << building->m_SittingRoom << endl;
-	cout << "好基友正在访问" << building->m_BedRoom << endl;
+	cout << "好基友1正在访问" << building->m_SittingRoom << endl;
+	cout << "好基友1正在访问" << building->m_BedRoom << endl;
+}
+
+void goodGay::visit2()
+{
+	cout << "好基友2正在访问" << building->m_SittingRoom << endl;
+	cout << "好基友2正在访问" << building->m_BedRoom << endl;
 }
 
 void test01()
 {
 	goodGay gg;
 	gg.visit();
-
+	gg.visit2();
 }
 
 int main(){
-
+    
 	test01();
 
-	system("pause");
 	return 0;
 }
